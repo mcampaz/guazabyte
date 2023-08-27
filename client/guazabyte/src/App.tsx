4bc48505d27365ea80588a63ceb60a7cdc22f69c
MARIO CAMPAZ <mcampaz@users.noreply.github.com>
2023-08-27 16:09:40 -0700

    [Add] Typescript configuration

---
+++ client/guazabyte/src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      
+       
+        
+      <h1>GuazaByte!!!</h1>  
+          
+      
+    </div>
+  );
+}
+
